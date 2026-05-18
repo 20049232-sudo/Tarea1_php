49e463e437e9a33ef67523f14ad15c9123fd0d04
20049232-sudo <20049232@clases.edu.sv>
2026-05-18 17:27:03 -0600

    Ejercicio 8
    En este ejercicio pedimos dos numeros y que muestre cual es menor

---
+++ menor.php
@@ -0,0 +1,9 @@
+<?php
+$numero1 = $_POST['numero1'];
+$numero2 = $_POST['numero2'];
+if ($numero1 < $numero2) {
+    echo "El numero menor es:" .$numero1;
+} elseif ($numero2 < $numero1) {
+echo "El numero menor es:" .$numero2;
+}    
+?>
